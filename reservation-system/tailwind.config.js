b4cce7a5fc04c92c5e7bb70e37b013ba8182f97b
kevalG11 <dsmkevalg@outlook.com>
2025-03-30 16:01:46 +0000

    Updated the variables, of the errors, configs

--- reservation-system/tailwind.config.js
+++ reservation-system/tailwind.config.js
@@ -1,13 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./src/**/*.{js,ts,jsx,tsx}",
-      "./src/components/ui/**/*.tsx",
-      "./node_modules/@shadcn/ui/**/*.js",
-    ],
-    theme: {
-      extend: {},
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+      fontFamily: {
+        sahrati: ['"Sahrati"', 'sans-serif'], // Custom font with a fallback
+      },
     },
-    plugins: [],
-  }
-  
+  },
+  plugins: [],
+};
